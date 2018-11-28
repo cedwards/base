bb4c4dce4a659213effd5310eff44cb41a1d768a
Christer Edwards <christer.edwards@gmail.com>
2018-11-27 17:42:17 -0700

    Initial Commit

---
+++ usr/local/share/licenses/ca_root_nss-3.40/catalog.mk
@@ -0,0 +1,5 @@
+_LICENSE=MPL20
+_LICENSE_NAME=Mozilla Public License version 2.0
+_LICENSE_PERMS=dist-mirror dist-sell pkg-mirror pkg-sell auto-accept
+_LICENSE_GROUPS=FSF OSI
+_LICENSE_DISTFILES=nss-3.40.tar.gz
