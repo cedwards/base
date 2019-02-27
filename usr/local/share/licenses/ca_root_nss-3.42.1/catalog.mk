2690a338343c9f158392b74c6fb333ff460bb602
Christer Edwards <christer.edwards@gmail.com>
2019-02-27 07:52:18 -0700

    updated ca_root_nss bootstrap; 3.42.1

--- usr/local/share/licenses/ca_root_nss-3.42.1/catalog.mk
+++ usr/local/share/licenses/ca_root_nss-3.42.1/catalog.mk
@@ -2,4 +2,4 @@ _LICENSE=MPL20
 _LICENSE_NAME=Mozilla Public License version 2.0
 _LICENSE_PERMS=dist-mirror dist-sell pkg-mirror pkg-sell auto-accept
 _LICENSE_GROUPS=FSF OSI
-_LICENSE_DISTFILES=nss-3.40.1.tar.gz
+_LICENSE_DISTFILES=nss-3.42.1.tar.gz
